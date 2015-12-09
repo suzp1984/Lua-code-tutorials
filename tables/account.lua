6df878d9a6659c56787c485553b2169716dbd5ff
suzp1984 <suzp1984@gmail.com>
2015-12-09 14:01:47 +0800

    add account inheriant

--- tables/account.lua
+++ tables/account.lua
@@ -15,6 +15,23 @@ function Account:withdraw(v)
    self.balance = self.balance - v
 end
 
+SpecialAccount = Account:new()
+
+function SpecialAccount:withdraw(v)
+   if v - self.balance >= self:getLimit() then
+      error("insufficient funds")
+   end
+
+   self.balance = self.balance - v
+end
+
+function SpecialAccount:getLimit()
+   return self.limit or 0
+end
+
+s = SpecialAccount:new{limit = 1000}
+
+
 a = Account:new{balance = 0}
 a:deposit(100)
 
