b9cfa8b1e92c8270048c0f4819e67fce3361e481
suzp1984 <suzp1984@gmail.com>
2015-12-09 13:47:33 +0800

    account update

--- tables/account.lua
+++ tables/account.lua
@@ -1,10 +1,23 @@
 Account = {balance = 0}
 
+function Account:new(o)
+   o = o or {}
+   setmetatable(o, self)
+   self.__index = self
+   return o
+end
+
+function Account:deposit(v)
+   self.balance = self.balance + v
+end
+
 function Account:withdraw(v)
    self.balance = self.balance - v
 end
 
-a = Account
+a = Account:new{balance = 0}
+a:deposit(100)
+
 a:withdraw(100)
 
 Account.withdraw(a, 10)
